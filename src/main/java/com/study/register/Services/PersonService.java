883fb022691502e66766fdaf97123c2b40a9fb93
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-05 18:07:10 -0300

    Created: Services - PersonService

--- src/main/java/com/study/register/Services/PersonService.java
+++ src/main/java/com/study/register/Services/PersonService.java
@@ -5,10 +5,28 @@
  */
 package com.study.register.Services;
 
+import com.study.register.entity.Person;
+import com.study.register.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+
 /**
  *
  * @author Samuel
  */
 public class PersonService {
     
+    private PersonRepository personRepository;
+    
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+    
+    public String CreatePerson(@RequestBody Person person) {
+        
+        Person savedPerson = personRepository.save(person);
+        
+        return "Created, ID - "+savedPerson.getId();
+    }
 }

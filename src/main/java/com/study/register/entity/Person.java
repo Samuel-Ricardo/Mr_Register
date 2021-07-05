4a8dc0c678bd633dc558319f854f68d4245be2dd
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-05 16:51:27 -0300

    Updated: Class - Person > Anotations

--- src/main/java/com/study/register/entity/Person.java
+++ src/main/java/com/study/register/entity/Person.java
@@ -7,8 +7,14 @@ package com.study.register.entity;
 
 import java.time.LocalDate;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,15 +33,32 @@ import lombok.NoArgsConstructor;
 public class Person {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @Column(nullable = false)
     private String fristName;
     
+    @Column(nullable = false)
     private String lastName;
     
+    @Column(
+            nullable = false, 
+            unique = true
+    )    
     private String cpf;
     
+    @Column(nullable = true)
     private LocalDate birthDate;
     
+    @Column(nullable = true)
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            cascade = {
+                CascadeType.PERSIST,
+                CascadeType.MERGE,
+                CascadeType.REMOVE
+            }
+    )
     private List<Phone> phones;
 }

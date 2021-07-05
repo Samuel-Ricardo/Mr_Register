04de22a4897b89064f1f36ab4302e65d21ad52c6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-05 16:34:00 -0300

    Updated: Class - Phone

--- src/main/java/com/study/register/entity/Phone.java
+++ src/main/java/com/study/register/entity/Phone.java
@@ -6,6 +6,13 @@
 package com.study.register.entity;
 
 import com.study.register.enums.PhoneType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,13 +23,21 @@ import lombok.NoArgsConstructor;
  * @author Samuel
  */
 
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PhoneType type;
+    
+    @Column(nullable = false)
     private String number;
 }

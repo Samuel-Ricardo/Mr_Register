2087b7062f9fd965752ee504c699ea541bf20149
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-05 16:40:13 -0300

    Updated: Class - Person

--- src/main/java/com/study/register/entity/Person.java
+++ src/main/java/com/study/register/entity/Person.java
@@ -5,10 +5,37 @@
  */
 package com.study.register.entity;
 
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author Samuel
  */
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     
+    @Id
+    private Long id;
+    
+    private String fristName;
+    
+    private String lastName;
+    
+    private String cpf;
+    
+    private LocalDate birthDate;
+    
+    private List<Phone> phones;
 }

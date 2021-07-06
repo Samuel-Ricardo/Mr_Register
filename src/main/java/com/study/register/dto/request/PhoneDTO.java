798b6c21d42715b72ea32e8ca45dfe9f2727656d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-06 09:12:17 -0300

    Created: DTO - PhoneDTO

--- src/main/java/com/study/register/dto/request/PhoneDTO.java
+++ src/main/java/com/study/register/dto/request/PhoneDTO.java
@@ -6,10 +6,10 @@
 package com.study.register.dto.request;
 
 import com.study.register.enums.PhoneType;
-import javax.persistence.Column;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-//import javax.validation.constraints;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,10 +29,9 @@ public class PhoneDTO {
     private long id;
     
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private PhoneType type;
     
-    @Column(nullable = false)
-//    @NotEmpty
+    @NotEmpty
+    @Size(min = 13, max = 14)
     private String number;
 }

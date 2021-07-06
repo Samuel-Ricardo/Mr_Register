7a29239d08d3eed2b6bd64a31b56dad50064371a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-06 10:33:10 -0300

    Refactor: Code to Function - verifyIfExist -> (PersonService)

--- src/main/java/com/study/register/Services/PersonService.java
+++ src/main/java/com/study/register/Services/PersonService.java
@@ -21,42 +21,49 @@ import org.springframework.stereotype.Service;
  *
  * @author Samuel
  */
-
 @Service
 public class PersonService {
-    
+
     private PersonRepository personRepository;
     private final PersonMapper personMapper = PersonMapper.ISNTANCE;
-    
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
-    
+
     public String CreatePerson(@RequestBody PersonDTO personDTO) {
-        
+
         Person person = personMapper.toModel(personDTO);
-        
+
         Person savedPerson = personRepository.save(person);
-        
-        return "Created, ID - "+savedPerson.getId();
+
+        return "Created, ID - " + savedPerson.getId();
     }
 
     public List<PersonDTO> listAll() {
         List<Person> allPeople = personRepository.findAll();
-        
+
         return allPeople.stream()
                 .map(personMapper::toDTO)
                 .collect(Collectors.toList());
     }
 
     public PersonDTO findById(long id) throws PersonNotFoundException {
-    
-        Person person = personRepository.findById(id).
-                orElseThrow(() -> new PersonNotFoundException(id));
-        
+
+        Person person = verifyIfExist(id);
+
         return personMapper.toDTO(person);
     }
-    
-    
+
+    public String deleteById(long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private Person verifyIfExist(long id) throws PersonNotFoundException {
+        Person person = personRepository.findById(id).
+                orElseThrow(() -> new PersonNotFoundException(id));
+        return person;
+    }
+
 }

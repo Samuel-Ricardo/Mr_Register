9cf088486d71b3c9965746ad7ab3fb98ef048720
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-06 12:12:50 -0300

    preparing environment to test

--- src/test/java/com/study/register/RegisterApplicationTests.java
+++ src/test/java/com/study/register/RegisterApplicationTests.java
@@ -1,7 +1,10 @@
 package com.study.register;
 
+import com.study.register.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +13,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @ExtendWith(MockitoExtension.class)
 class RegisterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
+    @Mock
+    private PersonRepository personRepository;
+    
+    @InjectMocks
+    private PersonService personService;
 }
